50169bbc3ed502bfb4e99891641b74d0246699f2
pengxiaoliang <crenewideas@163.com>
2022-11-17 17:47:05 +0800

    计算二叉搜索树的高度

--- myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyBinarySearchTree.java
+++ myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyBinarySearchTree.java
@@ -8,13 +8,18 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.awt.color.ProfileDataException;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 //二叉搜索树
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class MyBinarySearchTree<E> implements MyTree<E>, BinaryTreeInfo {
 
     private int size;
@@ -54,12 +59,12 @@ public class MyBinarySearchTree<E> implements MyTree<E>, BinaryTreeInfo {
         //非根节点：
         Node<E> oneNode = rootNode;
         Node<E> parentNode = rootNode;
-        E rootElement = oneNode.element;
-        int comp = compareNodeElement(element, rootElement);
+        int comp = 0;
         while (oneNode != null){
+            comp = compareNodeElement(element, oneNode.element);
             parentNode = oneNode;
             if(comp < 0 ){
-                oneNode = oneNode .leftNode;
+                oneNode = oneNode.leftNode;
             }else if(comp > 0){
                 oneNode = oneNode.rightNode;
             }else {
@@ -118,6 +123,119 @@ public class MyBinarySearchTree<E> implements MyTree<E>, BinaryTreeInfo {
         if(element == null){
             throw new IllegalArgumentException("节点不能为空！");
         }
+    }
+
+    //前序遍历树
+    public void preorderTraversal(Node<E> rootNode, Consumer<E> consumer){
+        if(rootNode == null) return;
+        //回调函数，传什么，就对当前节点执行什么操作。
+        consumer.accept(rootNode.element);
+        //前序遍历左子树
+        preorderTraversal(rootNode.leftNode,consumer);
+        //前序遍历右子树
+        preorderTraversal(rootNode.rightNode,consumer);
+    }
+
+    //中序遍历树
+    public void interTraversal(Node<E> rootNode,Consumer<E> consumer){
+        if(rootNode == null) return ;
+        //1.遍历左子树。
+        interTraversal(rootNode.leftNode,consumer);
+        //2.回调函数执行。
+        consumer.accept(rootNode.element);
+        //3.遍历右子树。
+        interTraversal(rootNode.rightNode,consumer);
+    }
+
+    //后序遍历树，用 Function 函数接口，返回值代表是否需要继续遍历，来实现特定条件下停止继续遍历的功能。
+    //这种方式，可以实现要停止之后的递归调用都不会执行，效率比较高。
+    public boolean postTraversal(Node<E> rootNode, Function<E,Boolean> function){
+        //当叶子节点遍历完成，要遍历其子节点，这时，子节点 == null ,返回 false ，表示继续遍历其他节点。
+        if(rootNode == null) return false;
+        //1.遍历左子树。
+        boolean stopIfNecessary;
+        stopIfNecessary = postTraversal(rootNode.leftNode, function);
+        if(stopIfNecessary) return true;
+        //2.遍历右子树。
+        stopIfNecessary = postTraversal(rootNode.rightNode,function);
+        if(stopIfNecessary) return true;
+        //3.回调函数执行。
+        return function.apply(rootNode.element);
+    }
+
+    //层序遍历树,利用队列，出栈时，就将左右子树的节点添加到队列中。
+    //层序遍历的用处：1.计算二叉树的高度。见 levelOrderTraversalHeight()
+    public void levelOrderTraversal(Node<E> rootNode,Consumer<E> consumer){
+        Queue<Node<E>> oneQueue = new ConcurrentLinkedQueue<>();
+        oneQueue.add(rootNode);
+        while (!oneQueue.isEmpty()){
+            Node<E> pollNode = oneQueue.poll();
+            //回调函数执行。
+            consumer.accept(pollNode.element);
+            if(pollNode.leftNode != null){
+                oneQueue.add(pollNode.leftNode);
+            }
+            if(pollNode.rightNode != null){
+                oneQueue.add(pollNode.rightNode);
+            }
+        }
+    }
+
+    //利用层序遍历计算二叉树的高度
+    public int levelOrderTraversalHeight(){
+        Queue<Node<E>> oneQueue = new LinkedList<>();
+        oneQueue.add(rootNode);
+        //当前层弹出了多少个元素。
+        int count = 0 ;
+        //当前层元素的总个数。
+        int queueElementSize = 1;
+        //当前层的高度。
+        int height = 0;
+        while (!oneQueue.isEmpty()){
+            Node<E> pollNode = oneQueue.poll();
+            //每弹出一个元素，当前层的元素个数就 - 1；
+            queueElementSize --;
+            if(pollNode.leftNode != null){
+                oneQueue.add(pollNode.leftNode);
+            }
+            if(pollNode.rightNode != null){
+                oneQueue.add(pollNode.rightNode);
+            }
+            //当前层弹出了 queueElementSize 个元素时，说明当前层元素已经弹出完毕，那么高度就 + 1。同时将下一层的所有元素个数置为队列中的元素个数。
+            if(queueElementSize == 0){
+                queueElementSize = oneQueue.size();
+                height ++;
+            }
+        }
+        return height;
+    }
+
+    //利用层序遍历计算二叉树的高度
+    public int height(Node<E> node){
+        //如果是叶子结点 的左右子节点，则返回0。
+        if(node == null) return 0;
+        //计算当前节点的左子树高度
+        int leftHeight = height(node.leftNode);
+        //计算当前节点的右子树高度
+        int rightHeight = height(node.rightNode);
+        //当前节点的左右子树的高度都计算后，取二者的较大值 + 1，作为当前节点的高度。
+        int max = Math.max(leftHeight, rightHeight);
+        return max +1;
+    }
+
+    //打印当前二叉树
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        toString(rootNode,stringBuilder,"");
+        return stringBuilder.toString();
+    }
+
+    private void toString(Node<E> node,StringBuilder stringBuilder,String prefix){
+        if(node == null) return ;
+        stringBuilder.append(prefix ).append(node.element).append("\n");
+        toString(node.leftNode,stringBuilder,prefix + "【L】");
+        toString(node.rightNode,stringBuilder,prefix + "【R】");
     }
 
     @Override
--- myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyBinaryTreeMain.java
+++ myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyBinaryTreeMain.java
@@ -3,6 +3,9 @@ package cn.pxl.dataStructure.demo05BinarySearchTree;
 import cn.pxl.dataStructure.common.printer.LevelOrderPrinter;
 import cn.pxl.dataStructure.entity.Person;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class MyBinaryTreeMain {
 
     public static void main(String[] args) {
@@ -14,19 +17,49 @@ public class MyBinaryTreeMain {
         MyBinarySearchTree<Person> myBinarySearchTree = new MyBinarySearchTree<>();
         //自定义比较器的 二叉搜索树(年龄越小的，比较时按越大处理。)
         MyBinarySearchTree<Person> myComparatorBinarySearchTree = new MyBinarySearchTree<>((e1,e2)-> e2.getAge() - e1.getAge());
-        Person[] persons = new Person[10];
-        for (int i = 0; i < 10; i++) {
-            persons[i] = new Person(i +1);
-        }
+        Person[] persons = new Person[11];
+        persons[0] = new Person(7);
+        persons[1] = new Person(4);
+        persons[2] = new Person(2);
+        persons[3] = new Person(5);
+        persons[4] = new Person(1);
+        persons[5] = new Person(9);
+        persons[6] = new Person(8);
+        persons[7] = new Person(3);
+        persons[8] = new Person(11);
+        persons[9] = new Person(10);
+        persons[10] = new Person(12);
+
+
         for (Person person : persons) {
             myBinarySearchTree.add(person);
             myComparatorBinarySearchTree.add(person);
         }
 //        LevelOrderPrinter levelOrderPrinter = new LevelOrderPrinter(myBinarySearchTree);
 //        levelOrderPrinter.print();
-        LevelOrderPrinter levelOrderPrinter2 = new LevelOrderPrinter(myComparatorBinarySearchTree);
-        levelOrderPrinter2.print();
+//        LevelOrderPrinter levelOrderPrinter2 = new LevelOrderPrinter(myComparatorBinarySearchTree);
+//        levelOrderPrinter2.print();
+        LevelOrderPrinter levelOrderPrinter = new LevelOrderPrinter(myBinarySearchTree);
+        levelOrderPrinter.print();
+        System.out.println("");
 
+//测试停止功能是否成功
+//        myBinarySearchTree.postTraversal(myBinarySearchTree.getRootNode(),(person -> {
+//            System.out.println(person.getAge());
+//            return person.getAge() == 5;
+//        }));
+
+        //myBinarySearchTree.preorderTraversal(myBinarySearchTree.getRootNode(), System.out::println);
+        //myBinarySearchTree.interTraversal(myBinarySearchTree.getRootNode(), System.out::println);
+        //myBinarySearchTree.levelOrderTraversal(myBinarySearchTree.getRootNode(), System.out::println);
+
+        //测试打印字符串功能是否成功
+        //System.out.println(myBinarySearchTree);
+
+        //测试层序遍历方式获取树的高度。
+        System.out.println(myBinarySearchTree.height(myBinarySearchTree.getRootNode()));
+
+        System.out.println(myBinarySearchTree.levelOrderTraversalHeight());
     }
 
 
