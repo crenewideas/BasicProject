e4668739e8d34ea28062a1b12167a22f5c653925
pengxiaoliang <crenewideas@163.com>
2022-11-21 16:49:26 +0800

    红黑树代码02

--- myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyBinaryTreeMain.java
+++ myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyBinaryTreeMain.java
@@ -16,27 +16,30 @@ public class MyBinaryTreeMain {
         MyBinarySearchTree2<Person> myComparatorBinarySearchTree = new MyBinarySearchTree2<>((e1, e2)-> e2.getAge() - e1.getAge());
         //Avl树。
         MyBinarySearchTree2<Person> myAvlTree = new MyAvlTree<>();
+        MyBinarySearchTree2<Person> myRBTree = new MyRBTree<>();
+
         Person[] persons = new Person[15];
-        persons[0] = new Person(7);
-        persons[1] = new Person(4);
-        persons[2] = new Person(2);
-        persons[3] = new Person(5);
-        persons[4] = new Person(1);
-        persons[5] = new Person(9);
-        persons[6] = new Person(8);
-        persons[7] = new Person(3);
-        persons[8] = new Person(11);
-        persons[9] = new Person(10);
-        persons[10] = new Person(12);
-        persons[11] = new Person(14);
-        persons[12] = new Person(13);
-        persons[13] = new Person(15);
-        persons[14] = new Person(16);
+        persons[0] = new Person(67);
+        persons[1] = new Person(52);
+        persons[2] = new Person(92);
+        persons[3] = new Person(96);
+        persons[4] = new Person(53);
+        persons[5] = new Person(95);
+        persons[6] = new Person(13);
+        persons[7] = new Person(63);
+        persons[8] = new Person(34);
+        persons[9] = new Person(82);
+        persons[10] = new Person(76);
+        persons[11] = new Person(54);
+        persons[12] = new Person(9);
+        persons[13] = new Person(68);
+        persons[14] = new Person(39);
 
         for (Person person : persons) {
 //            myBinarySearchTree.add(person);
 //            myComparatorBinarySearchTree.add(person);
-            myAvlTree.add(person);
+//            myAvlTree.add(person);
+            myRBTree.add(person);
         }
 //        LevelOrderPrinter levelOrderPrinter = new LevelOrderPrinter(myBinarySearchTree);
 //        levelOrderPrinter.print();
@@ -50,6 +53,10 @@ public class MyBinaryTreeMain {
 //        LevelOrderPrinter levelOrderPrinter = new LevelOrderPrinter(myAvlTree);
 //        levelOrderPrinter.print();
 //        System.out.println("");
+        //测试RB树的自平衡特性
+        LevelOrderPrinter levelOrderPrinter = new LevelOrderPrinter(myRBTree);
+        levelOrderPrinter.print();
+        System.out.println("");
 
 
 //测试停止功能是否成功
--- myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyRBTree.java
+++ myAr/src/main/java/cn/pxl/dataStructure/demo05BinarySearchTree/MyRBTree.java
@@ -19,10 +19,113 @@ public class MyRBTree<E> extends MyBinarySearchTree2<E>{
 
     }
 
+    @Override
+    protected Node<E> createNode(E element, Node<E> parentNode) {
+        return new RBNode<E>(element,parentNode);
+    }
+
     //添加元素后，恢复整个树的平衡
+    //1.元素只可能添加到4阶B树的叶子节点。
+    //2.4阶B树的叶子节点的情况只可能有以下四种情况：RBR、RB、BR、B。（中间节点一定是黑色，左右红色节点会和中间的黑色节点合并形成一个B树节点）。
+    //3.对于B这种情况，直接添加，并且将节点染色为R即可（节点没有上溢，添加后也才2个节点）
+    //4.对于RB、BR来说，可以进行旋转，并将中间节点染色为B，左右节点染色为R。（节点没有上溢，添加后也才3个节点）
+    //5.对于RBR来说，B节点上溢，左右分为两棵子树，B节点上溢后，按新添加的节点处理。
     @Override
     protected void convertToBalanceTree(Node<E> node) {
+        if(node == null) return;
+        RBNode<E> currentNode = color(node,BLACK);
+        if(rootNode == null) {
+            //根节点染色为黑色
+            rootNode = currentNode;
+            return;
+        }
+        //当前节点父节点是空的，只需要保证当前节点是黑色即可，上方已经赋值为黑色，所以直接返回。
+        if(currentNode.parentNode == null){
+            return;
+        }
+        //当前节点的父节点是黑色，说明是B树中的 B 这种情况 ， 或者是 RB、BR 这两种情况中，向B节点添加元素的情况。
+        if (colorOfBlack(currentNode.parentNode)) {
+            //父结点为黑色，父结点的子节点染色为红色，即可满足红黑树的性质，黑色与当前红色可以合并为一个B树节点。
+            color(currentNode,RED);
+            return;
+        }
+        //这里，一定是添加节点的父结点是红色的情况。需要区分是 RBR，还是RB、BR。区分的依据是：uncle 节点是否为黑色。如果uncle节点为黑色，那么属于RB、BR的情况（uncle节点为 null 当然是黑色。）
+        //uncle节点是红色，属于RBR情况，需要处理上溢节点。
+        if (colorOfBlack(currentNode.uncle())) {
+            //到这里，分为四种子情况 ：  1:  B      2:  B       3:  B           4:  B
+            //                         R          R                 R              R
+            //                       C              C            C                     C
+            //其中，1和2需要右旋；3和4需要左旋。
 
+            //afterRotateNode 也就是旋转后的树的根节点（或者说是B树节点的BLACK节点）
+            RBNode<E> afterRotateNode;
+            //当前B树的root节点（或Black节点）。
+            Node<E> grandNode ;
+            //当前B树节点的父节点是不是左子节点。为空表示当前B树节点没有父节点。
+            Boolean currentSubTreeIsLeft = currentNode.parentNode.parentNode.parentNode== null ? null : currentNode.parentNode.parentNode.isLeftChild();
+            if (currentNode.parentNode.isRightChild()) {
+                //需要左旋 ，对应情况 3 和 4。
+                afterRotateNode = rotateLeft(currentNode);
+                grandNode = afterRotateNode.parentNode;
+            } else {
+                //需要右旋，对应情况 1 和 2。
+                afterRotateNode = rotateRight(currentNode);
+                grandNode = afterRotateNode.parentNode;
+            }
+
+            if (currentSubTreeIsLeft == null) {
+                //这里是这棵B树节点原本就没有父节点的情况。
+                afterRotateNode.parentNode = null;
+
+            }else if(currentSubTreeIsLeft){
+                //当前B树节点是父节点的左子树
+                grandNode.parentNode.leftNode = afterRotateNode;
+                afterRotateNode.parentNode = grandNode.parentNode;
+            }else {
+                //当前B树节点是父节点的右子树
+                grandNode.parentNode.rightNode = afterRotateNode;
+                afterRotateNode.parentNode = grandNode.parentNode;
+            }
+            grandNode.parentNode = afterRotateNode;
+        }else {
+            //这里是RBR情况。B节点直接上溢，左右R节点变成B节点即可。
+            black(currentNode.parentNode);
+            black(currentNode.uncle());
+            //将B节点上溢。也就是当前B节点作为参数进行递归即可。
+            convertToBalanceTree(currentNode.parentNode.parentNode);
+        }
+    }
+
+    private RBNode<E> rotateLeft(Node<E> currentNode){
+        Node<E> parentNode = currentNode.parentNode;
+        Node<E> grandParentNode = parentNode.parentNode;
+        grandParentNode.rightNode = parentNode.leftNode;
+        if(parentNode.leftNode != null) parentNode.leftNode.parentNode = grandParentNode;
+        parentNode.leftNode = grandParentNode;
+        //染色
+        //祖父节点变成了B树的左边节点，需要染成红色
+        red(grandParentNode);
+        //父结点变成了B树的中间节点，需要染成黑色
+        black(parentNode);
+        //当前节点变成了B树的右边节点，需要染成红色
+        red(currentNode);
+        return (RBNode<E>) parentNode;
+    }
+
+    private RBNode<E> rotateRight(Node<E> currentNode){
+        Node<E> parentNode = currentNode.parentNode;
+        Node<E> grandParentNode = parentNode.parentNode;
+        grandParentNode.leftNode = parentNode.rightNode;
+        if(parentNode.rightNode != null) parentNode.rightNode.parentNode = grandParentNode;
+        parentNode.rightNode = grandParentNode;
+        //染色
+        //祖父节点变成了右边节点，需要染成红色
+        red(grandParentNode);
+        //父结点变成了中间节点，需要染成黑色
+        black(parentNode);
+        //当前节点变成了B树的右边节点，需要染成红色
+        red(currentNode);
+        return (RBNode<E>) parentNode;
     }
 
     //染色,传入染色节点及要染成的颜色。
@@ -38,7 +141,7 @@ public class MyRBTree<E> extends MyBinarySearchTree2<E>{
         return color(node,RED);
     }
 
-    //对当前节点染色成黑丝
+    //对当前节点染色成黑色
     private RBNode<E> black(Node<E> node){
         return color(node,BLACK);
     }
@@ -53,21 +156,34 @@ public class MyRBTree<E> extends MyBinarySearchTree2<E>{
         return node != null && ((RBNode<E>)node).color == RED;
     }
 
-
-    //当前节点是否是黑
+    //当前节点是否是黑，空节点也是黑色（叶子结点视为黑色）
     private boolean colorOfBlack(Node<E> node){
-        return node != null && ((RBNode<E>)node).color == BLACK;
+        return node == null || ((RBNode<E>)node).color == BLACK;
     }
 
 
+    @Override
+    public Object string(Object node) {
+        RBNode<E> rbNode = (RBNode<E>) node;
+        String str = "";
+        if(rbNode.color == BLACK) str ="B_";
+        return str + rbNode.element.toString();
+    }
 
     private static class RBNode<E> extends Node<E>{
-        boolean color;
+        boolean color = BLACK;
         public RBNode(E element, Node<E> parentNode) {
             super(element, parentNode);
         }
         public RBNode(E element, Node<E> parentNode, Node<E> leftNode, Node<E> rightNode) {
             super(element, parentNode, leftNode, rightNode);
+        }
+
+        @Override
+        public String toString() {
+            String str = "";
+            if(color == BLACK) str ="B_";
+            return str + element.toString();
         }
     }
 }
