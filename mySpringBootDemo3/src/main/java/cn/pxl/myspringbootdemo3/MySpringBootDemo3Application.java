1c15c364e6ce6d025131678d04ff548257ee6fc9
pxl <crenewideas@163.com>
2023-01-06 17:18:02 +0800

    mySpringBootDemo3提交

---
+++ mySpringBootDemo3/src/main/java/cn/pxl/myspringbootdemo3/MySpringBootDemo3Application.java
@@ -0,0 +1,13 @@
+package cn.pxl.myspringbootdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootDemo3Application.class, args);
+    }
+
+}
---
+++ mySpringBootDemo3/src/test/java/cn/pxl/myspringbootdemo3/MySpringBootDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.pxl.myspringbootdemo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootDemo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
